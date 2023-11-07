83ef27b7c395bfb4c4f2f9860beb27f2eec442ca
Jaba <jabaqadagishvili4@gmail.com>
2023-11-07 20:14:43 +0400

    the errorMessage component was created and the RandomChar component was modified. now error img/gif is displayed in case of server error

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -3,6 +3,7 @@ import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 import { MarvelService } from "../../services/MarvelService";
 import { Spinner } from "../spinner/Spinner";
+import { ErrorMessage } from "../errorMessage/ErrorMessage";
 
 class RandomChar extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class RandomChar extends Component {
     //the state may then contain some other data, not only this: { name: null, description: null, thumbnail: null, homepage: null, wiki: null}.  That's why, I put all this data into a separate Object that will characterize the character
     char: {},
     loading: true,
+    error: false,
   };
 
   marvelService = new MarvelService(); //in order to work with the JS class, we create an instance
@@ -21,18 +23,30 @@ class RandomChar extends Component {
     this.setState({ char, loading: false }); // same as ({char : char})
   };
 
+  onError = () => {
+    this.setState({ loading: false, error: true });
+  };
+
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    this.marvelService.getCharacter(id).then(this.onCharLoaded); //the argument that came from this .then() function will be written to char: onCharLoaded = (char) here
+    this.marvelService
+      .getCharacter(id)
+      .then(this.onCharLoaded) //the argument that came from .then()  will be written to char: onCharLoaded = (char)
+      .catch(this.onError);
   };
   //Now after that, it is necessary to call the updateChar method, so I will call it in the constructor in a moment.
 
   render() {
-    const { char, loading } = this.state; //since these states are already inside the 'char' object, we’ll just do this kind of destructuring
+    const { char, loading, error } = this.state; //since these states are already inside the 'char' object, we’ll just do this kind of destructuring
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View char={char} /> : null; //if there is no loading now or there is no error, then in this case we will return the View component
 
     return (
       <div className="randomchar">
-        {loading ? <Spinner /> : <View char={char} />}
+        {errorMessage}
+        {spinner}
+        {content}
         <div className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!
--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -2,7 +2,13 @@ const Spinner = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      style={{ margin: "0 auto", background: "none", display: "block" }}
+      style={{
+        margin: "0 auto",
+        background: "none",
+        display: "block",
+        width: "50px",
+        height: "50px",
+      }}
       width="200px"
       height="200px"
       viewBox="0 0 100 100"
