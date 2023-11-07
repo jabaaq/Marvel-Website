8f6f3b8d64c0068ff2955f37dbb80d8126cd394c
Jaba <jabaqadagishvili4@gmail.com>
2023-11-07 18:20:33 +0400

    added the loader into RandomChar component

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 import { MarvelService } from "../../services/MarvelService";
+import { Spinner } from "../spinner/Spinner";
 
 class RandomChar extends Component {
   constructor(props) {
@@ -9,14 +10,15 @@ class RandomChar extends Component {
     this.updateChar();
   }
   state = {
-    char: {},
     //the state may then contain some other data, not only this: { name: null, description: null, thumbnail: null, homepage: null, wiki: null}.  That's why, I put all this data into a separate Object that will characterize the character
+    char: {},
+    loading: true,
   };
 
   marvelService = new MarvelService(); //in order to work with the JS class, we create an instance
 
   onCharLoaded = (char) => {
-    this.setState({ char }); // same as ({char : char})
+    this.setState({ char, loading: false }); // same as ({char : char})
   };
 
   updateChar = () => {
@@ -26,31 +28,11 @@ class RandomChar extends Component {
   //Now after that, it is necessary to call the updateChar method, so I will call it in the constructor in a moment.
 
   render() {
-    const {
-      char: { name, description, thumbnail, homepage, wiki },
-    } = this.state; //since these states are already inside the 'char' object, we’ll just do this kind of destructuring
+    const { char, loading } = this.state; //since these states are already inside the 'char' object, we’ll just do this kind of destructuring
 
     return (
       <div className="randomchar">
-        <div className="randomchar__block">
-          <img
-            src={thumbnail}
-            alt="Random character"
-            className="randomchar__img"
-          />
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">{description}</p>
-            <div className="randomchar__btns">
-              <a href={homepage} className="button button__main">
-                <div className="inner">homepage</div>
-              </a>
-              <a href={wiki} className="button button__secondary">
-                <div className="inner">Wiki</div>
-              </a>
-            </div>
-          </div>
-        </div>
+        {loading ? <Spinner /> : <View char={char} />}
         <div className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!
@@ -67,5 +49,27 @@ class RandomChar extends Component {
     );
   }
 }
+
+const View = ({ char }) => {
+  const { thumbnail, description, name, homepage, wiki } = char;
+
+  return (
+    <div className="randomchar__block">
+      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <div className="randomchar__info">
+        <p className="randomchar__name">{name}</p>
+        <p className="randomchar__descr">{description}</p>
+        <div className="randomchar__btns">
+          <a href={homepage} className="button button__main">
+            <div className="inner">homepage</div>
+          </a>
+          <a href={wiki} className="button button__secondary">
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RandomChar;
