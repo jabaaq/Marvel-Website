c88e07121a56dbad81f1798af217bf191f6a8440
Jaba <jabaqadagishvili4@gmail.com>
2023-11-27 18:08:59 +0400

    code refactoring - turned the CharInfo class component into a functional component

--- src/components/charInfo/CharInfo.jsx
+++ src/components/charInfo/CharInfo.jsx
@@ -5,70 +5,71 @@ import {ErrorMessage} from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 import {MarvelService} from '../../services/MarvelService';
 import PropTypes from 'prop-types';
+import {useEffect, useState} from 'react';
 
-class CharInfo extends Component {
-  state = {
-    char: null,
-    loading: false,
-    error: false,
-  };
+const CharInfo = (props) => {
+  const [char, setChar] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  marvelService = new MarvelService();
+  const marvelService = new MarvelService();
 
-  componentDidMount() {
-    this.updateChar();
-  }
+  useEffect(() => {
+    updateChar();
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.charId !== prevProps.charId) {
-      this.updateChar();
-    }
-  }
+  useEffect(() => {
+    updateChar();
+  }, [props.charId]);
 
-  updateChar = () => {
-    const {charId} = this.props;
+  // componentDidUpdate(prevProps) {
+  //   if (this.props.charId !== prevProps.charId) {
+  //     this.updateChar();
+  //   }
+  // }
+
+  const updateChar = () => {
+    const {charId} = props;
 
     if (!charId) {
       return;
     }
 
-    this.onCharLoading(); //so that before the request we will show a spinner
-    this.marvelService //and if there is already an ID, then I make a request to the server
+    onCharLoading(); //so that before the request we will show a spinner
+    marvelService //and if there is already an ID, then I make a request to the server
       .getCharacter(charId)
-      .then(this.onCharLoaded) //when a response arrives from the service in the format of one object with a character, it will be taken as a char argument and written to state
-      .catch(this.onError);
+      .then(onCharLoaded) //when a response arrives from the service in the format of one object with a character, it will be taken as a char argument and written to state
+      .catch(onError);
   };
 
-  onCharLoaded = (char) => {
-    this.setState({char, loading: false});
+  const onCharLoaded = (char) => {
+    setChar(char);
+    setLoading(false);
   };
 
-  onCharLoading = () => {
-    this.setState({loading: true});
+  const onCharLoading = () => {
+    setLoading(true);
   };
 
-  onError = () => {
-    this.setState({loading: false, error: true});
+  const onError = () => {
+    setLoading(false);
+    setError(true);
   };
 
-  render() {
-    const {char, loading, error} = this.state;
+  const skeleton = char || loading || error ? null : <Skeleton />; //If any of this exists, then we don’t render anything.
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error || !char) ? <View char={char} /> : null; //It says here that it is not loading, not an error, and there is already a character. so I wrote: !char
 
-    const skeleton = char || loading || error ? null : <Skeleton />; //If any of this exists, then we don’t render anything.
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error || !char) ? <View char={char} /> : null; //It says here that it is not loading, not an error, and there is already a character. so I wrote: !char
-
-    return (
-      <div className="char__info">
-        {skeleton}
-        {errorMessage}
-        {spinner}
-        {content}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="char__info">
+      {skeleton}
+      {errorMessage}
+      {spinner}
+      {content}
+    </div>
+  );
+};
 
 const View = ({char}) => {
   //for the convenience of working with this large component from the rendering method, it was divided into two different components, one of them will deal only with the interface, the other with logic and state
