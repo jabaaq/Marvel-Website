6784cf783680ade4e94091c2c9d2beafe2999cdb
Jaba <jabaqadagishvili4@gmail.com>
2023-11-29 15:48:10 +0400

    since there are already several pages on the site, React Router has been added. Now the customer can navigate between pages

--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -1,21 +1,22 @@
+import {Link} from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <a href="#">
+        <Link to="/">
           <span>Marvel</span> Information Portal
-        </a>
+        </Link>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <a href="#">Characters</a>
+            <Link to="/">Characters</Link>
           </li>
           /
           <li>
-            <a href="#">Comics</a>
+            <Link to="/comics">Comics</Link>
           </li>
         </ul>
       </nav>
