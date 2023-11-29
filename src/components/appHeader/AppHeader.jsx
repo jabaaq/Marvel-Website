ec923b915f735740a4a4cacbff018b64b2798258
Jaba <jabaqadagishvili4@gmail.com>
2023-11-29 16:04:04 +0400

    several Link components have been replaced with NavLink components to style the title of the opened page

--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
@@ -12,11 +12,15 @@ const AppHeader = () => {
       <nav className="app__menu">
         <ul>
           <li>
-            <Link to="/">Characters</Link>
+            <NavLink exact activeStyle={{color: '#9f0013'}} to="/">
+              Characters
+            </NavLink>
           </li>
           /
           <li>
-            <Link to="/comics">Comics</Link>
+            <NavLink exact activeStyle={{color: '#9f0013'}} to="/comics">
+              Comics
+            </NavLink>
           </li>
         </ul>
       </nav>
