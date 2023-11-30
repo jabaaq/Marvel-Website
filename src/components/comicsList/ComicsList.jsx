556869b0c1072807f9745d3c0faf97504e04a7bc
Jaba <jabaqadagishvili4@gmail.com>
2023-11-30 14:03:59 +0400

    created 404 page, from which it is possible to return to the main page

--- src/components/comicsList/ComicsList.jsx
+++ src/components/comicsList/ComicsList.jsx
@@ -1,9 +1,6 @@
 import './comicsList.scss';
-import uw from '../../resources/img/UW.png';
-import xMen from '../../resources/img/x-men.png';
 import {useState, useEffect, useRef} from 'react';
 import {useMarvelService} from '../../services/MarvelService';
-import {ErrorMessage} from '../errorMessage/ErrorMessage';
 import {Spinner} from '../spinner/Spinner';
 
 const ComicsList = () => {
